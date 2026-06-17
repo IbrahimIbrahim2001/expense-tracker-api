5558e5f43e25fa8f4f16f170d1a00678ae5652e8
IbrahimIbrahim2001 <ii8538098@gmail.com>
2026-06-17 11:21:32 +0300

    feat: add validation schema for login API in auth router

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import AuthController from "./auth.controller.ts";
 import { validate } from "../../middleware/validation.ts";
-import { registerSchema, updateUserProfileSchema } from "./auth.validation.ts";
+import { loginSchema, registerSchema, updateUserProfileSchema } from "./auth.validation.ts";
 import { verifyToken } from "../../middleware/verifyToken.ts";
 
 const router: Router = Router();
@@ -14,7 +14,7 @@ router.post("/register", validate(registerSchema), AuthController.register);
 // Login user
 // POST
 // /api/auth/login
-router.post("/login", AuthController.login);
+router.post("/login", validate(loginSchema), AuthController.login);
 
 // Get current user
 // GET
